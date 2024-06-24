334dee9ad0f93b53ed8938fc84c9efb7ff992695
arctic1106 <arcticsoft@outlook.com>
2024-06-24 07:50:16 +0200

    Finished.

--- src/main/java/com/arcticsoft/Application.java
+++ src/main/java/com/arcticsoft/Application.java
@@ -1,14 +1,15 @@
 package com.arcticsoft;
 
 public class Application {
-
+    
     public static void main(String[] args) throws Exception {
-        var fromPage = 11;
-        var toPage = 12;
+        System.setProperty("java.util.logging.config.file", "src/main/resources/logging.properties"); 
+        var fromPage = 1;
+        var toPage = 200;
         if (args.length == 2) {
             fromPage = Integer.parseInt(args[0]);
             toPage = Integer.parseInt(args[1]);
         }
-        ImageService.downloadNPages(fromPage, toPage);
+        ImageService.downloadPages(fromPage, toPage, true);
     }
 }
--- src/main/java/com/arcticsoft/ImageService.java
+++ src/main/java/com/arcticsoft/ImageService.java
@@ -7,60 +7,65 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ImageService {
     private static final Logger LOGGER = Logger.getLogger(ImageService.class.getSimpleName());
-    private static final Path imagesDir = IOService.createDirectory("images");
+    private static final Path IMAGES_DIR;
+
+    static {
+        try {
+            IMAGES_DIR = Files.createDirectories(Paths.get("images"));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
     private ImageService() {
     }
 
-    public static void downloadNPages(int fromPage, int toPage) {
-        for (var i = fromPage; i <= toPage; i++) {
-            var links = HtmlParser.getLinksPagina(i);
-            LOGGER.log(Level.INFO, "\nPágina {0}: {1} Links encontrados\n", new Object[] { i, links.size() });
+    public static void downloadPages(int fromPage, int toPage, boolean virtualThreads) {
+        if (virtualThreads)
+            downloadPagesConcurrent(fromPage, toPage);
+        else
+            downloadPagesSequential(fromPage, toPage);
+    }
+
+    private static void downloadPagesSequential(int fromPage, int toPage) {
+        for (var pageNumber = fromPage; pageNumber <= toPage; pageNumber++) {
+            var links = HtmlParser.getLinksPagina(pageNumber);
             for (var link : links)
                 downloadImage(HtmlParser.obtenerImagenURI(link));
-            LOGGER.log(Level.INFO, "\nPágina {0} completada\n", i);
         }
     }
 
-    public static void downloadNPagesConcurrent(int fromPage, int toPage) {
-        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
-            for (var i = fromPage; i <= toPage; i++) {
-                final int pageNumber = i;
-                executor.submit(() -> downloadPageConcurrent(pageNumber));
+    private static void downloadPagesConcurrent(int fromPage, int toPage) {
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor();) {
+            for (var pageNumber = fromPage; pageNumber <= toPage; pageNumber++) {
+                var finalPageNumber = pageNumber;
+                executor.submit(() -> {
+                    var links = HtmlParser.getLinksPagina(finalPageNumber);
+                    for (var link : links)
+                        downloadImage(HtmlParser.obtenerImagenURI(link));
+                });
             }
         }
-    }
-
-    private static void downloadPageConcurrent(int pageNumber) {
-        var links = HtmlParser.getLinksPagina(pageNumber);
-        LOGGER.log(Level.INFO, "\nPágina {0}: {1} Links encontrados\n", new Object[] { pageNumber, links.size() });
-
-        try (ExecutorService imageExecutor = Executors.newVirtualThreadPerTaskExecutor()) {
-            for (var link : links) {
-                imageExecutor.submit(() -> downloadImage(HtmlParser.obtenerImagenURI(link)));
-            }
-        }
-        LOGGER.log(Level.INFO, "\nPágina {0} completada\n", pageNumber);
     }
 
     private static void downloadImage(final URI uri) {
         var fileName = Paths.get(uri.getPath()).getFileName().toString();
-        var imagePath = imagesDir.resolve(fileName);
+        var imagePath = IMAGES_DIR.resolve(fileName);
         if (Files.exists(imagePath)) {
-            LOGGER.log(Level.INFO, "Imagen {0} ya existe, omitiendo descarga\n", fileName);
+            LOGGER.log(Level.INFO, "Imagen {0} ya existe, omitiendo descarga.", fileName);
             return;
         }
         try {
             var data = uri.toURL().openStream().readAllBytes();
             Files.write(imagePath, data, StandardOpenOption.CREATE);
-            LOGGER.log(Level.INFO, "Imagen {0} descargada\n", fileName);
+            LOGGER.log(Level.INFO, "Imagen {0} descargada desde thread: {1}.",
+                    new Object[] { fileName, Thread.currentThread().threadId() });
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
